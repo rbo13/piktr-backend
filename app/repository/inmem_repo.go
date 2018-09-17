b1ec0cf5f81faefb0912ab31118567e5bf592ccb
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-09-17 21:23:03 +0800

    Added app/repository
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

---
+++ app/repository/inmem_repo.go
@@ -0,0 +1,81 @@
+package repository
+
+import (
+	"errors"
+	"piktr/app/model"
+)
+
+var (
+	errNotInserted  = errors.New("User not inserted")
+	errNotUpdated   = errors.New("User not updated")
+	errIDIsRequired = errors.New("ID is required")
+	errNotFound     = errors.New("User not found")
+)
+
+// InMemRepo is the concrete implementation
+// of the interface.
+// Saving it to memory
+type InMemRepo struct {
+	data map[int64]*model.User
+}
+
+// NewInMemRepo create new repository
+func NewInMemRepo() *InMemRepo {
+	var data = map[int64]*model.User{}
+	return &InMemRepo{
+		data: data,
+	}
+}
+
+// Create ...
+func (r *InMemRepo) Create(user *model.User) (*model.User, error) {
+	if user == nil {
+		return nil, errNotInserted
+	}
+	r.data[user.ID] = user
+	return r.data[user.ID], nil
+}
+
+// FindByID ...
+func (r *InMemRepo) FindByID(id int64) (*model.User, error) {
+	if id <= 0 {
+		return nil, errIDIsRequired
+	}
+
+	if r.data[id] == nil {
+		return nil, errNotFound
+	}
+	return r.data[id], nil
+}
+
+// FindAll ...
+func (r *InMemRepo) FindAll() ([]*model.User, error) {
+	var d []*model.User
+	for _, j := range r.data {
+		d = append(d, j)
+	}
+	return d, nil
+}
+
+// Update ...
+func (r *InMemRepo) Update(user *model.User) (*model.User, error) {
+	if user == nil {
+		return nil, errNotUpdated
+	}
+
+	if r.data[user.ID] == user {
+		r.data[user.ID] = user
+		return r.data[user.ID], nil
+	}
+
+	return nil, errNotFound
+}
+
+// Delete ...
+func (r *InMemRepo) Delete(id int64) error {
+	if id <= 0 {
+		return errIDIsRequired
+	}
+	r.data[id] = nil
+	return nil
+}
