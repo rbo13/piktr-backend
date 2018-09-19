c22e3084c0a885b5e2e49f1e965fc9881d62ef1f
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-09-19 18:42:47 +0800

    Added call to db package
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

--- main.go
+++ main.go
@@ -2,15 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 
 	"piktr/server"
 
+	"piktr/app/repository"
 	"piktr/app/response"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var (
@@ -19,7 +23,19 @@ var (
 	privKey       = "./certs/key.local.pem"
 )
 
+var gormDB *gorm.DB
+
+func init() {
+	gormDB, err := gorm.Open("mysql", "user:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer gormDB.Close()
+}
+
 func main() {
+	sqlDB := repository.NewSQLDb(gormDB)
+
 	r := chi.NewRouter()
 	r.Use(middleware.RequestID)
 	r.Use(middleware.RealIP)
@@ -27,6 +43,8 @@ func main() {
 	r.Use(middleware.Recoverer)
 
 	srv := server.New(serverAddress, r)
+
+	log.Print(sqlDB)
 
 	srv.Router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8;")
