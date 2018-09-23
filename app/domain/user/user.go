d2602acd430c3b5e0ec30affdba530df2ef4f9cf
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-09-23 21:33:15 +0800

    Changed DeletedAt to be pointer type
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

--- app/domain/user/user.go
+++ app/domain/user/user.go
@@ -7,13 +7,13 @@ import (
 // User represents
 // a user of our application
 type User struct {
-	ID        int64     `json:"id" db:"id"`
-	Email     string    `json:"email" db:"email"`
-	Username  string    `json:"username" db:"username"`
-	Password  string    `json:"-" db:"password"`
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt time.Time `json:"updated_at" db:"update_at"`
-	DeletedAt time.Time `json:"deleted_at" db:"deleted_at"`
+	ID        int64      `json:"id" db:"id"`
+	Email     string     `json:"email" db:"email"`
+	Username  string     `json:"username" db:"username"`
+	Password  string     `json:"-" db:"password"`
+	CreatedAt time.Time  `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at" db:"update_at"`
+	DeletedAt *time.Time `json:"deleted_at" db:"deleted_at"`
 }
 
 // TableName returns a string
