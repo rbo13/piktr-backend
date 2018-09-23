7b5b52ed5dc7e4231ea79aebd4e0e6bbfc78d0c1
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-09-23 21:44:02 +0800

    Added unique_key for users table
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

--- app/domain/user/user.go
+++ app/domain/user/user.go
@@ -8,8 +8,8 @@ import (
 // a user of our application
 type User struct {
 	ID        int64      `json:"id" db:"id"`
-	Email     string     `json:"email" db:"email"`
-	Username  string     `json:"username" db:"username"`
+	Email     string     `json:"email" sql:"type:varchar(100)" gorm:"unique_index" db:"email"`
+	Username  string     `json:"username" sql:"type:varchar(50)" gorm:"unique_index" db:"username"`
 	Password  string     `json:"-" db:"password"`
 	CreatedAt time.Time  `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time  `json:"updated_at" db:"update_at"`
