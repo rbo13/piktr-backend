a506de21f88e71cec35a10ec438b2db172fedcc7
Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>
2018-09-27 22:53:24 +0800

    Fixed response.JSON
    Signed-off-by: Richard Burk Orofeo <chardy.orofeo@yahoo.com.ph>

--- app/response/json.go
+++ app/response/json.go
@@ -1,20 +1,39 @@
 package response
 
 import (
+	"encoding/json"
 	"net/http"
+)
+
+// MIME types
+const (
+	MIMEApplicationJSON            = "application/json"
+	MIMEApplicationJSONCharsetUTF8 = MIMEApplicationJSON + "; " + charsetUTF8
+)
+
+// character set
+const (
+	charsetUTF8 = "charset=UTF-8"
 )
 
 // JSON returns the v type
 // as a json response
 // by adding a content-type
 // as application/json
-func JSON(w http.ResponseWriter, v []byte) error {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	_, err := w.Write(v)
+func JSON(statusCode int, w http.ResponseWriter, v interface{}) error {
+	b, err := json.Marshal(v)
 
 	if err != nil {
 		return err
 	}
+
+	w.Header().Set("Content-Type", MIMEApplicationJSONCharsetUTF8)
+	w.WriteHeader(statusCode)
+	_, err = w.Write(b)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
