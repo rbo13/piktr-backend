57b74f87833d0100a3221ba3e731699a016282b8
Richard Orofeo <chardy.orofeo@yahoo.com.ph>
2018-10-13 11:09:25 +0800

    Added test inmemory for finding all users

--- app/inmemory/inmemory_test.go
+++ app/inmemory/inmemory_test.go
@@ -13,40 +13,52 @@ func init() {
 }
 
 func TestCreateUser(t *testing.T) {
-
 	// arrange
 	user := &user.User{
-		ID:       int64(1),
-		Email:    "sam@smith.com",
-		Username: "sam_smith",
-		Password: "notsecurepassword",
+		ID:    int64(1),
+		Email: "sam@smith.com", Username: "sam_smith", Password: "notsecurepassword",
 	}
-
 	// act
 	err := userRepo.Create(user)
-
 	// assert
 	if err != nil {
 		t.Errorf("Error creating user: %v", err)
 	}
 }
-
 func TestGetUserByID(t *testing.T) {
-
 	// arrange
 	id := int64(1)
-
 	// act
 	user, err := userRepo.FindByID(id)
-
 	// assert
 	if user == nil {
 		t.Errorf("No user found by that ID: %v", user)
 	}
-
 	if err != nil {
 		t.Errorf("Error finding a user: %v", err)
 	}
 
-	t.Log(user)
+	if user != nil {
+		t.Log(user)	
+	}
+
+}
+func TestFindAllUsers(t *testing.T) {
+	// arrange
+
+	// act
+	users, err := userRepo.FindAll()
+	// assert
+	if err != nil {
+		t.Errorf("Error occurred due to: %v", err)
+	}
+	if users == nil {
+		t.Errorf("Users cannot be empty: %v", users)
+	}
+	if len(users) <= 0 {
+		t.Errorf("Users length is: %d", len(users))
+	}
+	if len(users) > 0 {
+		t.Log(&users)
+	}
 }
